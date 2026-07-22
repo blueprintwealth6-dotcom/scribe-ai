2844310fda8ad07cac0d8fd63cd64d9e8683f84e
blueprintwealth6-dotcom <blueprintwealth6@gmail.com>
2026-07-22 09:39:03 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -63,3 +63,8 @@ app.get('/api/health', (req, res) => {
 });
 
 export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
